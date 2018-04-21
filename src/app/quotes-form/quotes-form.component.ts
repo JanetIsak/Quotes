326230c95ed27d5f5050c13853724a3c2c8fcac4
Janet Isak <janetisak@yahoo.com>
2018-04-21 20:45:42 +0300

    add ngModel to get two-way data binding

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+
+newQuotes=new Quotes("","",new Date());
 
   constructor() { }
 
